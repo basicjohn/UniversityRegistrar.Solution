5d2833850745ad10460dc194f58e704b486ed42b
basicjohn <edmondsonj@gmail.com>
2021-05-24 12:05:33 -0700

    add project build
    Co-authored-by: Sammai Gutierrez <sammaigtz@hotmail.com>

---
+++ UniversityRegistrar/Models/Course.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace UniversityRegistrar.Models
+{
+  public class Course
+  {
+    public Course()
+    {
+      this.JoinEntities = new HashSet<CourseStudent>();
+    }
+
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    public string CourseNumber { get; set; }
+    public string Description { get; set; }
+    public string CourseSchedule { get; set; }
+    public string TeacherName { get; set; }
+    public virtual ICollection<CourseStudent> JoinEntities { get; set; }
+  }
+}
